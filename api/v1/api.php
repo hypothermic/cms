d0840d5c74f55fcd36c4e1d0a2cb2e74a5e6c27e
hypothermic <admin@hypothermic.nl>
2019-09-18 13:18:35 +0200

    API info endpoint
    Signed-off-by: hypothermic <admin@hypothermic.nl>

---
+++ api/v1/api.php
@@ -0,0 +1,24 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+http_response_code(200);
+
+$client = array(
+    "name" => "Wide World Importers",
+    "logo" => "img/logo/"
+);
+
+$result = array(
+    "version" => "v1.0.0.0", // TODO
+    "impl"    => "standard",
+    "client"  => $client,
+);
+
+print(json_encode(
+    array(
+        "return"  => "info",
+        "info"    => $result
+    )
+));
