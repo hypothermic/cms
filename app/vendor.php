7fa8e40801680b427f9e64349d02696c154392e2
hypothermic <admin@hypothermic.nl>
2019-10-31 13:45:10 +0100

    Maak vendor bestand aan met constants
    Signed-off-by: hypothermic <admin@hypothermic.nl>

---
+++ app/vendor.php
@@ -0,0 +1,8 @@
+<?php
+
+const VENDOR_NAME                   = "Wide World Importers", // website titel
+      VENDOR_DESCRIPTION            = "Welkom op onze webshop!", // korte omschrijving
+      VENDOR_THEME_COLOR_PRIMARY    = "#0095ff", // primare kleur
+      VENDOR_THEME_COLOR_SECONDARY  = "#ff9900", // accenten kleur
+      VENDOR_THEME_COLOR_BACKGROUND = "#fafafa", // achtergrond kleur
+      VENDOR_THEME_COLOR_SHADOW     = "#000000"; // schaduw kleur
---
+++ index.php
@@ -0,0 +1,45 @@
+<?
+include_once("app/vendor.php");
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+
+        <!-- De beschrijving en eigenschappen van onze pagina -->
+        <title><? echo VENDOR_NAME ?></title>
+        <meta name="description" content="<? echo VENDOR_DESCRIPTION ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="<? echo VENDOR_THEME_COLOR_PRIMARY?>">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+
+        <!-- Normalize hebben we nodig voor het schoonmaken van pagina zodat het er in alle browsers hetzelfde uit ziet -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+
+
+        <!-- Alle JavaScript dependencies-->
+        <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Bootstrap -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- Onze website werkt niet met Internet Explorer 7 en lager-->
+        <!--[if IE]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please upgrade your browser to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Binnen deze tags komt de inhoud van deze webpagina. -->
+        <div>
+
+        </div>
+
+    </body>
+</html>
