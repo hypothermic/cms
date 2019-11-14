899289046ee25db3ab7532b6310d8dcd5d027853
Jeroen Stek <s1146515@student.windesheim.nl>
2019-11-14 14:15:38 +0100

    404 page not found gemaakt htacces aangepast

--- css/main.css.php
+++ css/main.css.php
@@ -4,4 +4,12 @@ header('Content-Type: text/css');
 
 // Uit vendor.php zijn we de constanten nodig.
    include_once "../app/vendor.php";
+
+
 ?>
+
+.Big-button{
+    width: 500px;
+    height: 200px;
+    font-size: 40px;
+}
